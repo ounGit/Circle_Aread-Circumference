8732853ae9600d080938f4e1a230f79eaf5cd5d5
oun.admin <oun.admin@IT-OUN>
2024-02-27 09:01:27 +0700

    Add project files.

---
+++ Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter radius of Circle: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Your radius you enter is: {radius}");
+            double area = Math.PI * radius * radius;
+            double circum = 2 * Math.PI * radius;
+            area = Math.Round(area, 2);
+            circum = Math.Round(circum, 2); 
+            Console.WriteLine($"Area of circle is : {area}");
+            Console.WriteLine($"Circumference {circum}");
+        }
+    }
+}
